1682b1ca78cf42df9c9f6778f1ddf0cd662dfc17
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2025-11-26 21:14:20 +0700

    bro just works

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -22,5 +22,5 @@ async function bootstrap() {
   const port = config.port;
   await app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
-bootstrap();
+// bootstrap();
 export default bootstrap;
