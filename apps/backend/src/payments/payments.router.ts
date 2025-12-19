d5a97cf4babe25f29a9a21d9503943e3ed32389b
Putnea <lamgialac2004@gmail.com>
2025-12-19 20:40:37 +0700

    update router middleware for reports and payments

---
+++ apps/backend/src/payments/payments.router.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { TrpcService } from 'src/trpc/trpc.service';
+import { PaymentsService } from './payments.service';
+import { UserRoleEnum } from 'src/entities/users.entity';
+import { PaymentSearchDto } from '@repo/shared';
+
+@Injectable()
+export class PaymentsRouter {
+    constructor(
+        private readonly trpcService: TrpcService,
+        private readonly paymentsService: PaymentsService,
+    ) { }
+
+    apply() {
+        Logger.log('Initialized paths /trpc/payments', 'PaymentsRouter');
+        return this.trpcService.router({
+            search: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .input(PaymentSearchDto)
+                .query(({ input }) => {
+                    return this.paymentsService.searchPayments(input);
+                }),
+        });
+    }
+}
---
+++ apps/backend/src/reports/reports.router.ts
@@ -0,0 +1,48 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { TrpcService } from 'src/trpc/trpc.service';
+import { ReportsService } from './reports.service';
+import { UserRoleEnum } from 'src/entities/users.entity';
+
+@Injectable()
+export class ReportsRouter {
+    constructor(
+        private readonly trpcService: TrpcService,
+        private readonly reportsService: ReportsService,
+    ) { }
+
+    apply() {
+        Logger.log('Initialized paths /trpc/reports', 'ReportsRouter');
+        return this.trpcService.router({
+            getOverview: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getOverview();
+                }),
+            getTodayRevenue: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getTodayRevenue();
+                }),
+            getLast30DaysRevenue: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getLast30DaysRevenue();
+                }),
+            getLast30DaysBookings: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getLast30DaysBookings();
+                }),
+            getTopRoutes: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getTopRoutes(5);
+                }),
+            getDailyRevenue: this.trpcService.procedure
+                .use(this.trpcService.roleGuardMiddleware(UserRoleEnum.ADMIN))
+                .query(() => {
+                    return this.reportsService.getDailyRevenue();
+                }),
+        });
+    }
+}
